999ed4eaca202e3030e289570214fef26cd2f3c4
meinreno <g.meinreno@yahoo.com.br>
2016-10-20 22:55:45 -0200

    Dando acesso para cadastro de novo usuario em outra tela

--- cadastrar_novo.php
+++ cadastrar_novo.php
@@ -1,3 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Cadastro de Novo Usuario</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+</head>
+<body>
+	<form name="novo_usuario" method="POST" action="#"> <!-- Form de Cadastro de Novo usuario, Apos curto cadastro sem erros irá para a pagina já logada-->
+		<h3>Novo Usuario? Preencha as informações para cadastro</h3>
+		Nome: <input type="text" name="novo_nome"><br>
+		Sobrenome: <input type="text" name="novo_sobrenome"><br>
+		E-Mail: <input type="text" name="novo_email"><br>
+		Senha: <input type="password" name="novo_senha"><br>
+		<input type="submit" name="Enviar">
+	</form>
+</body>
+</html>
 <?php
 
-echo "deu certo";
+	if(isset($_POST['novo_nome'])){
+		include 'Classes/Usuarios.php';
+
+		$novoUsuario = new novoUsuario($_POST['novo_nome'], $_POST['novo_sobrenome'], $_POST['novo_email'], $_POST['novo_senha']);
+
+		$novoUsuario->cadastrar_novoUsuario();
+
+		
+	}
--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
 	<title>Pedidos Online</title>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-	<script type="text/javascript" src="Scripts/jquery.js"></script>
+	<!--<script type="text/javascript" src="Scripts/jquery.js"></script>-->
 </head>
 <body>
 	<form name="login_usuario" method="POST" action="#"><!--Form para login para usuarios já cadastro-->
@@ -13,14 +13,7 @@
 		<input type="submit" name="Enviar">
 	</form>
 
-	<form name="novo_usuario" method="POST" action="#"> <!-- Form de Cadastro de Novo usuario, Apos curto cadastro sem erros irá para a pagina já logada-->
-		<h3>Novo Usuario? Preencha as informações para cadastro</h3>
-		Nome: <input type="text" name="novo_nome"><br>
-		Sobrenome: <input type="text" name="novo_sobrenome"><br>
-		E-Mail: <input type="text" name="novo_email"><br>
-		Senha: <input type="password" name="novo_senha"><br>
-		<input type="submit" name="Enviar">
-	</form>
+	
 
 <script type="text/javascript">
 
@@ -34,17 +27,7 @@
 
 <?php 
 	
-	if(isset($_POST['novo_nome'])){
-		include 'Classes/Usuarios.php';
-
-		$novoUsuario = new novoUsuario($_POST['novo_nome'], $_POST['novo_sobrenome'], $_POST['novo_email'], $_POST['novo_senha']);
-
-		$novoUsuario->cadastrar_novoUsuario();
-
-		
-	}
-
-		if(isset($_POST['login_email'])){
+	if(isset($_POST['login_email'])){
 		include 'Classes/Usuarios.php';
 
 		$loginUsuario = new LoginUsuario($_POST['login_email'], $_POST['login_senha']);
